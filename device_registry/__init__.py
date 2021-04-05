e8a0568775b31af89268a4c324d50e06206ff221
kareemassad <kareemassad5@gmail.com>
2021-04-04 21:41:22 -0400

    init new portions of project

--- device_registry/__init__.py
+++ device_registry/__init__.py
@@ -32,7 +32,9 @@ def index():
     """Present some documentation"""
 
     # Open the README file
-    with open(os.path.dirname(app.root_path) + "/README.md", "r") as markdown_file:
+    with open(
+        os.path.dirname(app.root_path) + "/device_registry/README.md", "r"
+    ) as markdown_file:
 
         # Read the content of the file
         content = markdown_file.read()
